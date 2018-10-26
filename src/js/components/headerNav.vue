650a1b92a5066daf3891f49bb946f424e7b90f10
Jakubem <jakubmx@gmail.com>
2018-10-26 12:45:28 +0200

    create empty vue components

---
+++ src/js/components/headerNav.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+      };
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../../css/_var.scss';
+</style>
