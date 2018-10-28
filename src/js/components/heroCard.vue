28a206d8a04d95ed7015735141cca1539ed915b6
Jakubem <jakubmx@gmail.com>
2018-10-28 19:39:30 +0100

    add content for mobile support

--- src/js/components/heroCard.vue
+++ src/js/components/heroCard.vue
@@ -12,6 +12,9 @@
       <p class="hero__copy">
         {{ heroCopy }}
       </p>
+      <p class="hero__copy hero__copy--mobile">
+        {{ heroCopyMobile }}
+      </p>
       <div class="button-group">
         <button-base :content="'get started now'"></button-base>
         <button-base class="button--mobile-hidden" :content="'learn more'"></button-base>
@@ -40,7 +43,7 @@
       ButtonBase,
       LineSeparator
     },
-    props: ['heroIntro', 'headline', 'heroCopy', 'src', 'fallback', 'mobileSrc'],
+    props: ['heroIntro', 'headline', 'heroCopy', 'heroCopyMobile', 'src', 'fallback', 'mobileSrc'],
     data: function () {
       return {
       };
@@ -54,14 +57,14 @@
   .hero__content {
     width: 100%;
     height: 100%;
-    margin-top: 100px;
+    margin-top: 150px;
     display: flex;
     flex-direction: column;
     align-items: center;
     position: absolute;
   }
 
-  .hero__intro {    
+  .hero__intro {
     font: 400 24px 'Source Sans Pro';
     color: $c-white;
     margin: 0 15px 15px 15px;
@@ -70,16 +73,33 @@
   .hero__headline {
     font: bold 72px 'Open Sans';
     color: $c-white;
-    margin: 0 15px 15px 15px;
+    margin: 0 15px 10px 15px;
     text-transform: uppercase;
     text-align: center;
   }
 
   .hero__copy {
-    font: 400 14px 'Open Sans';
+    font: 300 14px 'Open Sans';
+    line-height: 24px;
     color: $c-white;
     max-width: 600px;
-    margin: 0 15px 15px 15px;
+    margin: 20px 15px 65px 15px;
     text-align: center;
   }
+
+  .hero__copy--mobile {
+    display: none;
+  }
+
+  @media screen and (max-width: 700px) {
+
+    .hero__copy.hero__copy--mobile {
+      display: inline;
+    }
+
+    .hero__copy {
+      display: none;
+    }
+  }
+
 </style>
