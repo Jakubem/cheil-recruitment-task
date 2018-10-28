ac6142b1fa329ebf325079abacb427996a2f2a6d
Jakubem <jakubmx@gmail.com>
2018-10-28 17:06:11 +0100

    add rotating border on hover

--- src/js/components/singleCard.vue
+++ src/js/components/singleCard.vue
@@ -37,24 +37,52 @@
   @import '../../css/_var.scss';
   .single-card {
     margin: 20px;
+    margin-bottom: 110px;
     display: flex;
     flex-direction: column;
     align-items: center;
   }
 
   .single-card__image {
-    height: 73px;
-    width: 73px;
     display: flex;
+    position: relative;
     justify-content: center;
     align-items: center;
-    border: 1px solid $c-gray__light;
-    border-radius: $border-radius;
+    margin-bottom: 25px;
+    transition: .2s;
+    will-change: transform;
+    box-sizing: border-box;
+    padding: 0;
+    height: 73px;
+    width: 73px;
+    &::after {  
+      content: '';
+      display: block;    
+      position:absolute;
+      transition: .2s;
+      will-change: transform;
+      left:50%;
+      top:50%;
+      right:0;
+      bottom:0;
+      margin-left: -50%;
+      margin-top: -50%;
+      padding: 0;
+      border: 1px solid $c-gray__light;
+      border-radius: $border-radius;
+    }
+    &:hover::after {
+      transform: rotate(45deg);
+      border-color: $c-red;
+    }
+    &:hover + .single-card__headline {
+      color: $c-red;
+    }
   }
 
   .single-card__headline {
-    color: $c-gray;
-    font: bold 14px 'Open Sans';
+    color: $c-gray__text;
+    font: 600 18px 'Open Sans';
     text-transform: uppercase;
     letter-spacing: 0.01em;
     margin: 0 15px 15px 15px;
@@ -66,7 +94,17 @@
       width: 42px;
       height: 1px;
       margin: 0 auto;
+      margin-top: 15px;
       background-color: $c-gray;
     }
+  }
+
+  .single-card__copy {
+    text-align: center;
+    color: $c-gray__text;
+    font: 300 12px 'Source Sans Pro';
+    line-height: 20px;
+    width: 250px;
+    margin: 0;
   }
 </style>
