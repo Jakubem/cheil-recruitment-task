f58366137b8e36802ab4b40dcf154fba6cc7a0db
Jakubem <jakubmx@gmail.com>
2018-10-29 08:27:33 +0100

    add coments in entry point

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+
+// import style entry
 import '../css/style.scss';
+
+// polyfill for <picture> tag
 import './picturefill'
+
+// polyfill for promise on IE
 import 'promise-polyfill'
 
+// load top level componentes
 import HeroCarousel from './components/heroCarousel.vue';
 import CardRow from './components/cardRow.vue';
 
